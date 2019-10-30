ca8b8745aced066406f7f82bc257540d90336984
davidcolor164 <davidcolor164@gmail.com>
2019-10-30 16:12:04 +0700

    demo redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import Message from './components/Message';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        {/* <!-- Main --> */}
+        <main id="mainContainer">
+            <div className="container">
+               <Products />
+               <Message />
+                <Cart />
+                </div>
+        </main>
+        {/* <!-- Footer --> */}
+        <Footer />
+        </div>
+    );
+  }
 }
 
 export default App;
